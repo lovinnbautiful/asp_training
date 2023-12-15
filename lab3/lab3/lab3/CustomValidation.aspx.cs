76f730e7f406beca78f2f0c4f6872024dac8002f
lovinnbautiful <quangvinh072003@gmail.com>
2023-12-15 19:21:57 +0700

    com

--- lab3/lab3/lab3/CustomValidation.aspx.cs
+++ lab3/lab3/lab3/CustomValidation.aspx.cs
@@ -11,9 +11,8 @@ namespace lab3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)//load lần đầu
+            if (!IsPostBack)
             {
-                // sinh số ngẫu nhiên, hiển thị và lưu vào session
                 Random r = new Random();
                 lblMaBM.Text = r.Next(10000, 99999).ToString();
                 Session["Captcha"] = lblMaBM.Text;
@@ -21,7 +20,6 @@ namespace lab3
         }
         protected void MaBM_ServerValidate(object source, ServerValidateEventArgs args)
         {
-            // kiểm tra mã nhập vào và số ngẫu nhiên đã lưu trong session trước đó
             args.IsValid = (args.Value == Session["Captcha"].ToString());
         }
         protected void btnKiemTra_Click(object sender, EventArgs e)
